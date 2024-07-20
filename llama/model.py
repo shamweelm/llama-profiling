9d0aff1a1e9d299e0157e3df1f04be17a25913c2
shamweelm <mohdshamweel@gmail.com>
2024-07-20 22:29:00 +0100

    meta device fix

--- llama/model.py
+++ llama/model.py
@@ -210,7 +210,7 @@ class Attention(nn.Module):
                 self.n_local_kv_heads,
                 self.head_dim,
             )
-        ).cuda()
+        )
         self.cache_v = torch.zeros(
             (
                 args.max_batch_size,
@@ -218,7 +218,7 @@ class Attention(nn.Module):
                 self.n_local_kv_heads,
                 self.head_dim,
             )
-        ).cuda()
+        )
 
     def forward(
         self,
